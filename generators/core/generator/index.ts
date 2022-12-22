36947f977dda33193aa0ceaf01edf06edeb72a55
Felipe Bambirra <lipex360@gmail.com>
2022-12-22 07:13:07 -0300

    plop updated

---
+++ generators/core/generator/index.ts
@@ -0,0 +1,29 @@
+import { CreateFilesProps, generatorHandler } from "../handles"
+
+export default {
+  description: 'New Plop Generator',
+
+  prompts: [
+    {
+      type: 'input',
+      name: 'moduleName',
+      message: 'Module Name',
+      validate: (value: string) => (!value ? 'Value is required' : true),
+    },
+  ],
+
+  actions: () => {
+    const basePath = './components/{{moduleName}}'
+    const templatePath = './core/generator/templates'
+
+    const createFiles: CreateFilesProps[] = [
+      {
+        path: `${basePath}/index.ts`,
+        templateFile: `${templatePath}/index.hbs`,
+      }
+    ]
+
+    return generatorHandler({createFiles})
+  },
+
+}
--- generators/plopfile.ts
+++ generators/plopfile.ts
@@ -1,8 +1,11 @@
 import { NodePlopAPI } from 'plop'
+
+import plopGenerator from './core/generator'
 import components from './modules/components'
 import routes from './modules/routes'
 
 export default function (plop: NodePlopAPI) {
+  plop.setGenerator('PlopGenerator', plopGenerator)
   plop.setGenerator('Component', components)
   plop.setGenerator('Route', routes)
 }
